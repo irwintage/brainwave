35dd6a06ead8854d7b648078a7e637900b1884b1
irwintage <irwintage@gmail.com>
2026-03-18 23:35:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,36 @@
-const CACHE = "brainwave-v1";
+const CACHE = "brainwave-v2";
+
 const ASSETS = [
+  "./",
   "./index.html",
   "./manifest.json",
+
   "./fonts/cinzel-v26-latin-regular.woff2",
   "./fonts/cinzel-v26-latin-500.woff2",
   "./fonts/inter-v20-latin-300.woff2",
   "./fonts/inter-v20-latin-regular.woff2",
   "./fonts/inter-v20-latin-500.woff2"
 ];
- 
+
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
- 
+
 self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
- 
+
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
- 
